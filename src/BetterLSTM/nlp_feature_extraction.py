d7268dc1dc620ef21eac31862d5bbbbd1c1f0a61
WuZhuoran <munoliver007@gmail.com>
2017-12-17 19:32:10 -0400

    fix: update missing update

--- src/BetterLSTM/nlp_feature_extraction.py
+++ src/BetterLSTM/nlp_feature_extraction.py
@@ -2,11 +2,16 @@ import re
 
 import distance
 import pandas as pd
+import numpy as np
+import gensim
 from fuzzywuzzy import fuzz
 from nltk.corpus import stopwords
+from nltk import word_tokenize
 
 SAFE_DIV = 0.0001
 STOP_WORDS = stopwords.words("english")
+model = gensim.models.KeyedVectors.load_word2vec_format('../../input/glove.840B.300d.txt')
+norm_model = gensim.models.KeyedVectors.load_word2vec_format('../../input/glove.840B.300d.txt')
 
 
 def preprocess(x):
@@ -20,6 +25,64 @@ def preprocess(x):
         .replace("€", " euro ").replace("'ll", " will")
     x = re.sub(r"([0-9]+)000000", r"\1m", x)
     x = re.sub(r"([0-9]+)000", r"\1k", x)
+    x = re.sub('[“”\(\'…\)\!\^\"\.;:,\-\?？\{\}\[\]\\/\*@]', ' ', x)
+    x = re.sub(r"([0-9]+)000000", r"\1m", x)
+    x = re.sub(r"([0-9]+)000", r"\1k", x)
+    x = re.sub(r"[^A-Za-z0-9]", " ", x)
+    x = re.sub(r"what's", "", x)
+    x = re.sub(r"What's", "", x)
+    x = re.sub(r"\'s", " ", x)
+    x = re.sub(r"\'ve", " have ", x)
+    x = re.sub(r"can't", "cannot ", x)
+    x = re.sub(r"n't", " not ", x)
+    x = re.sub(r"I'm", "I am", x)
+    x = re.sub(r" m ", " am ", x)
+    x = re.sub(r"\'re", " are ", x)
+    x = re.sub(r"\'d", " would ", x)
+    x = re.sub(r"\'ll", " will ", x)
+    x = re.sub(r"60k", " 60000 ", x)
+    x = re.sub(r" e g ", " eg ", x)
+    x = re.sub(r" b g ", " bg ", x)
+    x = re.sub(r"\0s", "0", x)
+    x = re.sub(r" 9 11 ", "911", x)
+    x = re.sub(r"e-mail", "email", x)
+    x = re.sub(r"\s{2,}", " ", x)
+    x = re.sub(r"quikly", "quickly", x)
+    x = re.sub(r" usa ", " America ", x)
+    x = re.sub(r" USA ", " America ", x)
+    x = re.sub(r" u s ", " America ", x)
+    x = re.sub(r" uk ", " England ", x)
+    x = re.sub(r" UK ", " England ", x)
+    x = re.sub(r"india", "India", x)
+    x = re.sub(r"switzerland", "Switzerland", x)
+    x = re.sub(r"china", "China", x)
+    x = re.sub(r"chinese", "Chinese", x)
+    x = re.sub(r"imrovement", "improvement", x)
+    x = re.sub(r"intially", "initially", x)
+    x = re.sub(r"quora", "Quora", x)
+    x = re.sub(r" dms ", "direct messages ", x)
+    x = re.sub(r"demonitization", "demonetization", x)
+    x = re.sub(r"actived", "active", x)
+    x = re.sub(r"kms", " kilometers ", x)
+    x = re.sub(r"KMs", " kilometers ", x)
+    x = re.sub(r" cs ", " computer science ", x)
+    x = re.sub(r" upvotes ", " up votes ", x)
+    x = re.sub(r" iPhone ", " phone ", x)
+    x = re.sub(r"\0rs ", " rs ", x)
+    x = re.sub(r"calender", "calendar", x)
+    x = re.sub(r"ios", "operating system", x)
+    x = re.sub(r"gps", "GPS", x)
+    x = re.sub(r"gst", "GST", x)
+    x = re.sub(r"programing", "programming", x)
+    x = re.sub(r"bestfriend", "best friend", x)
+    x = re.sub(r"dna", "DNA", x)
+    x = re.sub(r"III", "3", x)
+    x = re.sub(r"the US", "America", x)
+    x = re.sub(r"Astrology", "astrology", x)
+    x = re.sub(r"Method", "method", x)
+    x = re.sub(r"Find", "find", x)
+    x = re.sub(r"banglore", "Banglore", x)
+    x = re.sub(r" J K ", " JK ", x)
     return x
 
 
@@ -87,6 +150,40 @@ def extract_features(df):
     df["fuzz_partial_ratio"] = df.apply(lambda x: fuzz.partial_ratio(x["question1"], x["question2"]), axis=1)
     df["longest_substr_ratio"] = df.apply(lambda x: get_longest_substr_ratio(x["question1"], x["question2"]), axis=1)
     return df
+
+
+def wmd(s1, s2):
+    s1 = str(s1).lower().split()
+    s2 = str(s2).lower().split()
+    stop_words = stopwords.words('english')
+    s1 = [w for w in s1 if w not in stop_words]
+    s2 = [w for w in s2 if w not in stop_words]
+    return model.wmdistance(s1, s2)
+
+
+def norm_wmd(s1, s2):
+    s1 = str(s1).lower().split()
+    s2 = str(s2).lower().split()
+    stop_words = stopwords.words('english')
+    s1 = [w for w in s1 if w not in stop_words]
+    s2 = [w for w in s2 if w not in stop_words]
+    return norm_model.wmdistance(s1, s2)
+
+
+def sent2vec(s):
+    words = str(s).lower()
+    words = word_tokenize(words)
+    words = [w for w in words if not w in STOP_WORDS]
+    words = [w for w in words if w.isalpha()]
+    M = []
+    for w in words:
+        try:
+            M.append(model[w])
+        except:
+            continue
+    M = np.array(M)
+    v = M.sum(axis=0)
+    return v / np.sqrt((v ** 2).sum())
 
 
 print("Extracting features for train:")
